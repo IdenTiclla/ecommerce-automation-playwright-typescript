5213ee063df0cede07fbf7564d9d9a784976c5b5
Iden Ticlla <iden.ticlla@gmail.com>
2026-08-08 15:43:37 -0400

    ci: publish Playwright test summary

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   // configuracion del reporter
   reporter: [
-    ['html', { outputFolder: 'playwright-report' }]
+    ['html', { outputFolder: 'playwright-report' }],
+    ['junit', { outputFile: 'test-results/results.xml' }]
   ],
   use: {
     headless: true,
